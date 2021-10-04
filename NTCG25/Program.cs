18a97d5168caf0b44a9b462981a569a1bd906150
NTCG25 <65662881+NTCG25@users.noreply.github.com>
2021-10-04 15:00:32 -0500

    Add files via upload

---
+++ NTCG25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NTCG25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many days you've been alive?: ");
+
+            string daysAlive = Console.ReadLine();
+            int num2 = Int32.Parse(daysAlive);
+
+            
+            Console.WriteLine("What is your level?");
+
+            string level = Console.ReadLine();
+            int num3 = Int32.Parse(level);
+
+            float lastnum = 1.2f;
+            
+            float Gamestage = num2 + num3 * lastnum;
+
+            Console.WriteLine("Your current Gamestage is " + Gamestage); 
+
+
+        }
+    }
+}
